66165ef4e57d31e372bdde019636993b62360ad9
Liaoxyu <Liaoxyu2023-01@outlook.com>
2025-06-02 18:00:43 +0800

    Rename Mark to Frac

--- advancedMath.py
+++ advancedMath.py
@@ -1,4 +1,4 @@
-class Mark: #分数
+class Frac: #分数
     def __init__(self, denominator: int, molecule: int) -> None: # ?分子和分母不可以是小数，所以参数类型只给int, 没给float. 但是参数先传分母，再传分子
         self.deno = denominator
         self.mole = molecule
@@ -11,12 +11,12 @@ def gcd(x: int, y: int) -> int: # 获取最大公因数的函数
         x, y = y, x % y
     return x
 
-def add(a: Mark, b: int | Mark) -> Mark: # 模拟分数的相加操作
+def add(a: Frac, b: int | Frac) -> Frac: # 模拟分数的相加操作
 
     if isinstance(b, int):
         # 将整数转换为分数
         # ! 不支持小数转分数, 参数b也只让传分数或整数，因为做小数转分数太难
-        b = Mark(1, b)
+        b = Frac(1, b)
 
     # 判断分数是不是同分子
     if a.deno != b.deno: # 如果不是
@@ -31,14 +31,14 @@ def add(a: Mark, b: int | Mark) -> Mark: # 模拟分数的相加操作
 
     commonnedMole: int = commonnedMoleWithA + commonnedMoleWithB
     # 返回结果且约分
-    return Mark(commonnedDeno // gcd(commonnedDeno, commonnedMole), commonnedMole // gcd(commonnedDeno, commonnedMole))
+    return Frac(commonnedDeno // gcd(commonnedDeno, commonnedMole), commonnedMole // gcd(commonnedDeno, commonnedMole))
 
-def minus(a: Mark, b: int | Mark) -> Mark: # 模拟分数的相减操作
+def minus(a: Frac, b: int | Frac) -> Frac: # 模拟分数的相减操作
 
     if isinstance(b, int):
         # 将整数转换为分数
         # ! 不支持小数转分数, 参数b也只让传分数或整数，因为做小数转分数太难
-        b = Mark(1, b)
+        b = Frac(1, b)
 
     # 判断分数是不是异分子
     if a.deno != b.deno: # 如果是
@@ -53,49 +53,49 @@ def minus(a: Mark, b: int | Mark) -> Mark: # 模拟分数的相减操作
 
     commonnedMole: int = commonnedMoleWithA - commonnedMoleWithB
     # 返回结果且约分
-    return Mark(commonnedDeno // gcd(commonnedDeno, commonnedMole), commonnedMole // gcd(commonnedDeno, commonnedMole))
+    return Frac(commonnedDeno // gcd(commonnedDeno, commonnedMole), commonnedMole // gcd(commonnedDeno, commonnedMole))
 
-def times(a: Mark, b: int | Mark) -> Mark: # 模拟分数的相乘操作
+def times(a: Frac, b: int | Frac) -> Frac: # 模拟分数的相乘操作
 
     if isinstance(b, int):
         # 将整数转换为分数
         # ! 不支持小数转分数, 参数b也只让传分数或整数，因为做小数转分数太难
-        newB = Mark(1, b)
+        newB = Frac(1, b)
     else:
         newB = b
     
     # 返回结果且约分
     if a.deno == newB.deno:
         mole = a.mole + newB.mole
-        return Mark(a.deno // gcd(a.deno, mole), mole // gcd(a.deno, mole))
+        return Frac(a.deno // gcd(a.deno, mole), mole // gcd(a.deno, mole))
     else:
         deno = a.deno*newB.deno
         mole = a.mole*newB.mole
-        return Mark(deno // gcd(deno, mole), mole // gcd(deno, mole))
+        return Frac(deno // gcd(deno, mole), mole // gcd(deno, mole))
 
 
-def divide(a: Mark, b: int | Mark) -> Mark: # 模拟分数的相除操作
+def divide(a: Frac, b: int | Frac) -> Frac: # 模拟分数的相除操作
 
     if isinstance(b, int):
         #将整数转换为分数，再转化为倒数
         # ! 不支持小数转分数, 参数b也只让传分数或整数，因为做小数转分数太难
-        newB = Mark(b, 1)
+        newB = Frac(b, 1)
     else:
-        newB = Mark(b.mole, b.deno)
+        newB = Frac(b.mole, b.deno)
     
     # 返回结果且约分
     if a.deno == newB.deno:
         mole = a.mole + newB.mole
-        return Mark(a.deno // gcd(a.deno, mole), mole // gcd(a.deno, mole))
+        return Frac(a.deno // gcd(a.deno, mole), mole // gcd(a.deno, mole))
     else:
         deno = a.deno*newB.deno
         mole = a.mole*newB.mole
-        return Mark(deno // gcd(deno, mole), mole // gcd(deno, mole))
+        return Frac(deno // gcd(deno, mole), mole // gcd(deno, mole))
 
 
 def main() -> None:
-    a = Mark(5, 2)
-    b = Mark(4, 10)
+    a = Frac(5, 2)
+    b = Frac(4, 10)
 
     #做一次分数运算！
     print(a, b)
